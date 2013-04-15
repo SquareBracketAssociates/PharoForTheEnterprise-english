0d6860a557c9c5cd3407d707988388e82760662f
Damien Cassou <damien.cassou@gmail.com>
2013-04-15 15:45:01 +0200

    Modularize compile.sh to ease hacking
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- compile.sh
+++ compile.sh
@@ -1,23 +1,70 @@
 #!/bin/bash
 
-chapters=$(cat PFTE.tex  | grep '^\\input' | grep -v common.tex | sed -e 's/^\\input{\([^}]*\)}.*$/\1/')
+VM_EXECUTABLE=./pharo
 
-for chapter in $chapters; do
-    echo =========================================================
-    echo COMPILING $chapter
-    echo =========================================================
+function generate_html() {
+    pier_source="$1"
+    $VM_EXECUTABLE Pharo.image eval <<EOF
+GutembergConsole generateStandaloneHTMLFromPier: '${pier_source}'.
+WorldState addDeferredUIMessage: [ SmalltalkImage current snapshot: true andQuit: true ].
+EOF
+}
 
-    # e.g., chapter = Zinc/Zinc.pier.tex
+function generate_latex() {
+    pier_source="$1"
+    $VM_EXECUTABLE Pharo.image eval <<EOF
+GutembergConsole generateSBALaTeXChapterFromPier: '${pier_source}'.
+WorldState addDeferredUIMessage: [ SmalltalkImage current snapshot: true andQuit: true ].
+EOF
+}
 
-    file=$(basename $chapter) # e.g., Zinc.pier.tex
-    file_pier=$(basename $chapter .tex) # e.g., Zinc.pier
-    dir=$(dirname $chapter) # e.g., Zinc
+function mypdflatex() {
+    pier_file="$1"
 
-    echo "GutembergConsole generateSBALaTeXChapterFromPier: '${dir}/${file_pier}'. WorldState addDeferredUIMessage: [ SmalltalkImage current snapshot: true andQuit: true ]." | ./vm.sh Pharo.image eval
-    echo "GutembergConsole generateStandaloneHTMLFromPier: '${dir}/${file_pier}'. WorldState addDeferredUIMessage: [ SmalltalkImage current snapshot: true andQuit: true ]." | ./vm.sh Pharo.image eval
+    pdflatex -halt-on-error -file-line-error -interaction batchmode "$pier_file"
 
-    cd $dir         # e.g., cd Zinc/
-    pdflatex $file
-    pdflatex $file
+}
+
+function produce_pdf() {
+    dir="$1"
+    pier_file="$2"
+
+    cd "$dir"         # e.g., cd Zinc/
+    mypdflatex "$pier_file" && mypdflatex "$pier_file"
     cd ..
-done
+}
+
+function compile() {
+    dir="$1"
+    pier_file="$2"
+    pier_source="${dir}/${pier_file}"
+    generate_html "$pier_source"
+    generate_latex "$pier_source"
+
+    produce_pdf "${dir}" "${pier_file}"
+}
+
+function compile_chapters() {
+    chapters=$(cat PFTE.tex  | grep '^\\input' | grep -v common.tex | sed -e 's/^\\input{\([^}]*\)}.*$/\1/')
+
+    for chapter in $chapters; do
+        echo =========================================================
+        echo COMPILING $chapter
+        echo =========================================================
+
+        # e.g., chapter = Zinc/Zinc.pier.tex
+
+        pier_file=$(basename $chapter .tex) # e.g., Zinc.pier
+        dir=$(dirname $chapter) # e.g., Zinc
+
+        compile "${dir}" "${pier_file}"
+    done
+}
+
+if [[ $# -eq 1 ]]; then
+    dir=$(dirname "$1") # e.g., Zinc
+    pier_file=$(basename "$1") # e.g., Zinc.pier
+    compile "${dir}" "${pier_file}"
+else
+    compile_chapters
+fi
