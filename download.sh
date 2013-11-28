4b3314f847d15ca579a40c14683cb543afc14b00
Damien Cassou <damien.cassou@gmail.com>
2013-11-28 14:05:24 +0100

    Use stable version of Pillar

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VM_INSTALL_URL="http://get.pharo.org/vm"
-IMAGE_URL="https://ci.inria.fr/pharo-contribution/job/Pillar/PHARO=20,VERSION=bleedingEdge,VM=vm/lastSuccessfulBuild/artifact/Pillar.zip"
+IMAGE_URL="https://ci.inria.fr/pharo-contribution/job/Pillar/PHARO=20,VERSION=stable,VM=vm/lastSuccessfulBuild/artifact/Pillar.zip"
 
 usage() {
     cat <<HELP
